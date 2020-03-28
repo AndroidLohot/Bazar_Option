a270ea70b72487266679be0c82098562fb6c2f3f
YogeshPc <developerhappy123@gmail.com>
2020-03-28 10:54:03 +0530

    Create only input output fragments

---
+++ app/src/main/java/com/example/bajaropetion/Input_Output_Fragment/Kaddhanye_Output.java
@@ -0,0 +1,29 @@
+package com.example.bajaropetion.Input_Output_Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bajaropetion.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Kaddhanye_Output extends Fragment {
+
+    public Kaddhanye_Output() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kaddhanye__output, container, false);
+    }
+}
